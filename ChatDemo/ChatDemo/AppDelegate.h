e031e8010d327d513e14b86bbc15a528830a5298
jia <mxr_jiang@server.local>
2016-07-14 11:43:53 +0800

    提交初始化项目

---
+++ ChatDemo/ChatDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChatDemo
+//
+//  Created by lj on 16/7/14.
+//  Copyright © 2016年 liujia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ChatDemo/ChatDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChatDemo
+//
+//  Created by lj on 16/7/14.
+//  Copyright © 2016年 liujia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
